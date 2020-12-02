89cfd325985bc0e12826b26d601c15075a403d21
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-12-02 09:09:54 -0700

    Added sweeps for linear and constant sweep.

--- final_project/variation_study.py
+++ final_project/variation_study.py
@@ -4,13 +4,18 @@ import machupX as mx
 import matplotlib.pyplot as plt
 import numpy as np
 
-def straight_swept_drag(sweep, taper, AR, alpha, grid):
-    """Calculates the induced drag on a straight swept wing.
+
+def swept_drag(sweep, sweep_type, taper, AR, CL, grid, max_iter=100, verbose=False):
+    """Calculates the induced drag on a swept wing at the given lift coef.
 
     Parameters
     ----------
-    sweep : float
+    sweep : float or list
         Sweep angle in degrees.
+
+    sweep_type : str
+        May be "constant", "linear", or "jointed". If "jointed", then
+        "sweep" should be a 2-element list.
 
     taper : float
         Taper ratio.
@@ -18,17 +23,26 @@ def straight_swept_drag(sweep, taper, AR, alpha, grid):
     AR : float
         Aspect ratio.
 
-    alpha : float
-        Angle of attack in degrees.
+    CL : float
+        Desired lift coefficient.
 
     grid : int
         Nodes per semispan to use in MachUpX.
+
+    max_iter : int, optional
+        Maximum iterations for the nonlinear solver in MachUpX. Defaults to 100.
+
+    verbose : bool, optional
+        Verbosity for MachUpX solver. Defaults to False.
 
     Returns
     -------
     CD_i : float
         Induced drag coefficient.
     """
+
+    # Calculate semispan
+    b_2 = 0.25*AR*(1.0+taper)
 
     # Create MachUpX input
     wing_dict = {
@@ -39,48 +53,155 @@ def straight_swept_drag(sweep, taper, AR, alpha, grid):
                 "ID" : 1,
                 "side" : "both",
                 "is_main" : True,
-                "semispan" : 1.5,
+                "semispan" : b_2,
                 "chord" : [[0.0, 1.0],
                            [1.0, taper]],
-                "sweep" : sweep,
                 "grid" : {
                     "N" : grid,
                 }
             }
         }
     }
+    if sweep_type=="constant":
+        wing_dict["wings"]["main_wing"]["sweep"] = sweep
+    elif sweep_type=="linear":
+        wing_dict["wings"]["main_wing"]["sweep"] = [[0.0, 0.0],
+                                                    [1.0, sweep]]
+    elif sweep_type=="jointed":
+        wing_dict["wings"]["main_wing"]["sweep"] = [[0.0, sweep[0]],
+                                                    [0.5, sweep[0]],
+                                                    [0.5, sweep[1]],
+                                                    [1.0, sweep[1]]]
 
     state = {
-        "velocity" : 1.0,
-        "alpha" : alpha
+        "velocity" : 1.0
     }
 
     # Initialize scene
-    scene = mx.Scene()
+    scene = mx.Scene(scene_input={"solver" : {"max_iterations" : max_iter}})
     scene.add_aircraft("wing", wing_dict, state=state)
 
     # Get induced drag
-    FM = scene.solve_forces(dimensional=False, body_frame=False)
+    scene.target_CL(CL=CL, set_state=True, verbose=verbose)
+    FM = scene.solve_forces(dimensional=False, body_frame=False, verbose=verbose)
     CD_i = FM["wing"]["total"]["CD"]
     return CD_i
 
 
-if __name__=="__main__":
+def sweep_and_taper_sweep(sweep_lims, N_sweeps, taper_lims, N_tapers, sweep_type, AR, CL_d, grid):
+    """Performs a sweep in taper ratio and sweep angle and returns an array of the induced drag coefficients.
 
-    # Straight swept wing
-    N_sweeps = 21
-    N_tapers = 6
-    sweeps = np.linspace(-30.0, 30.0, N_sweeps)
-    tapers = np.linspace(0.0, 1.0, N_tapers)
+    Parameters
+    ----------
+    sweep_lims : list
+        Limits of the sweep angle.
+
+    N_sweeps : int
+        Number of sweep angles to investigate.
+
+    taper_lims : list
+        Limits of the taper ratio.
+
+    N_tapers : int
+        Number of taper ratios to investigate.
+
+    sweep_type : str
+        Type of sweep to investigate
+
+    AR : float
+        Wing aspect ratio.
+
+    CL_d : float
+        Design lift coefficient.
+
+    grid : int
+        Nodes per semispan to use in MachUpX.
+
+    Returns
+    -------
+    tapers : ndarray
+        Array of taper ratios.
+
+    sweeps : ndarray
+        Array of sweep angles.
+
+    CD_i : ndarray
+        Array of induced drag coefficients.
+    """
+
+    # Initialize sweep variables
+    sweeps = np.linspace(sweep_lims[0], sweep_lims[1], N_sweeps)
+    tapers = np.linspace(taper_lims[0], taper_lims[1], N_tapers)
+
+    # Initialize storage
     CD_i = np.zeros((N_tapers, N_sweeps))
-    plt.figure()
+
+    # Sweep
     for i, taper in enumerate(tapers):
         for j, sweep in enumerate(sweeps):
 
-            CD_i[i,j] = straight_swept_drag(sweep, taper, 8.0, 5.0, 80)
+            # Get induced drag
+            CD_i[i,j] = swept_drag(sweep, sweep_type, taper, AR, CL_d, grid)
+
+    return tapers, sweeps, CD_i
+
+
+if __name__=="__main__":
+
+    # Investigate grid convergence for a straight swept wing
+
+    # Initialize plotting
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(8, 6))
+    axs = axs.flatten()
+    colors = ['#000000', '#222222', '#444444', '#666666', '#888888', '#AAAAAA', '#CCCCCC']
+    grids = [10, 20, 40, 80]
+
+    # Sweep
+    for k, grid in enumerate(grids):
+        
+        tapers, sweeps, CD_i = sweep_and_taper_sweep([-30.0, 30.0], 21, [0.0, 1.0], 6, "constant", 8.0, 0.5, grid)
 
         # Plot
-        plt.plot(sweeps, CD_i[i,:], 'k-')
+        for i, taper in enumerate(tapers):
+            axs[k].plot(sweeps, CD_i[i,:], label=str(round(taper, 2)), color=colors[i])
+
+        # Format subplot
+        if k==1:
+            axs[k].legend(title='Taper Ratio')
+        if k>1:
+            axs[k].set_xlabel("Sweep Angle [deg]")
+        if k%2==0:
+            axs[k].set_ylabel("CD_i")
+        axs[k].set_title(str(grid)+" Nodes per Semispan")
+
+    plt.savefig("final_project/plots/constant_sweep_and_taper_ratio_grid_study.png")
+
+
+    # Straight swept wing
+    
+    # Get drag sweep
+    tapers, sweeps, CD_i = sweep_and_taper_sweep([-30.0, 30.0], 21, [0.0, 1.0], 6, "constant", 8.0, 0.5, 80)
+
+    # Plot
+    plt.figure(figsize=(8, 6))
+    for i, taper in enumerate(tapers):
+        plt.plot(sweeps, CD_i[i,:], label=str(round(taper, 2)), color=colors[i])
     plt.xlabel("Sweep Angle [deg]")
-    plt.ylabel("Induced Drag Coefficient")
-    plt.show()
+    plt.ylabel("CD_i")
+    plt.legend()
+    plt.savefig("final_project/plots/constant_sweep_and_taper_ratio.png")
+
+
+    # Crescent wing
+    
+    # Get drag sweep
+    tapers, sweeps, CD_i = sweep_and_taper_sweep([-30.0, 30.0], 21, [0.0, 1.0], 6, "linear", 8.0, 0.5, 80)
+
+    # Plot
+    plt.figure(figsize=(8, 6))
+    for i, taper in enumerate(tapers):
+        plt.plot(sweeps, CD_i[i,:], label=str(round(taper, 2)), color=colors[i])
+    plt.xlabel("Sweep Angle [deg]")
+    plt.ylabel("CD_i")
+    plt.legend()
+    plt.savefig("final_project/plots/linear_sweep_and_taper_ratio.png")
